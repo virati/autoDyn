476eb614a90c974e82634074d497f9f7972bc0df
Vineet Tiruvadi <virati@gmail.com>
2020-11-24 16:59:33 -0500

    Big commit before I replace the dynSys class with a streamlined one I accidentally developed in LfLh repo

--- lib/dynLib.py
+++ lib/dynLib.py
@@ -12,17 +12,49 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import pdb
 import scipy.signal as sig
+from dynSys import dyn_sys
 
 # WC should be N regions x D elements - D = 2 for excitatory and inhibitory
-def wc_nmm(x,params):
-    L = params['L']
-    alpha = params['alpha']
-    beta = params['beta']
-    x_dot = np.zeros_like(x)
-    for ii in x.shape[0]:
-        x_dot[ii,0] = alpha[ii]*np.dot(L[ii,:],x[:,0]) - beta[ii]*x[ii,1]
+class WC_unit(dyn_sys):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
         
-    return x_dot
+    def fdyn(self,x,params):
+        wie = params['wie']
+        wei = params['wei']
+        wee = params['wee']
+        wii = params['wii']
+        
+        ze = params['ze']
+        zi = params['zi']
+        
+        x_dot = np.array([
+            - 1/wei * (np.log(1/(x[1] - 1)) - zi + wii * x[1]),
+            1/wie * (np.log(1/x[0] - 1) - ze + wee * x[0])
+            ])
+        
+        return x_dot
+    
+class WC_net(dyn_sys):
+    def __init__(self):
+        super().__init__(self)
+
+    def fdyn(self,x,params):
+        L = params['L']
+        alpha = params['alpha']
+        beta = params['beta']
+        
+        
 
 if __name__=='__main__':
-    pass
+    params = {'wie':1,
+             'wee':1,
+             'wii':1,
+             'wei':1,
+             'ze':1,
+             'zi':1,
+             'L':np.array([[1]])}
+    test_net = WC_unit(params=params)
+    x_init = np.array([1.0,1.0]).T
+    test_net.sim(x_i = x_init,t_end=10)
+    plt.figure();plt.plot(test_net.states().T)
--- lib/dynSys.py
+++ lib/dynSys.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+ #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Tue Feb 11 17:21:34 2020
@@ -12,7 +12,6 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import pdb
 import scipy.signal as sig
-from dynLib import *
 
 # Method to merge local and remote architectures
 
@@ -24,7 +23,7 @@ def kuramoto(x,params):
     D = params['D']
     w = params['w']
     k = params['k']
-    return w - k * np.dot(D,np.sin(np.dot(D.T,x)))
+    return w# - k * np.dot(D,np.cos(np.dot(D.T,x)))
 
 def rand_squar(x,params):
     x_dot = np.zeros_like(x)
@@ -45,21 +44,35 @@ class dyn_sys:
     dt = 0.001
     param_labels = ['L','C']
     
-    def __init__(self,params,dynamics,control,**kwargs):
-        self.params = params
+    def __init__(self,**kwargs):
+        self.params = kwargs['params']
         #have to have at least a connectivity L
-        self.L = params['L']
+        self.L = self.params['L']
        
         # bring in main methods
-        self.fdyn = dynamics
-        self.gctr = control
-    
+        #self.fdyn = dynamics
+        #self.gctr = control
+        
+    def set_dynamics(self,fn):
+        self.fdyn = fn
+        
     # Main Runge-Kutta Integrator
-    def integrator(self):
+    def integrator_ctrl(self):
         k1 = (self.fdyn(self.state,self.params) + self.gctr(self.u)) * self.dt
-        k2 = (self.fdyn(self.state + .5*k1,self.params)  + self.gctr(self.u))*self.dt
-        k3 = (self.fdyn(self.state + .5*k2,self.params)  + self.gctr(self.u))*self.dt
+        k2 = (self.fdyn(self.state + .5*k1,self.params)  + self.gctr(0.5*self.u))*self.dt
+        k3 = (self.fdyn(self.state + .5*k2,self.params)  + self.gctr(0.5*self.u))*self.dt
         k4 = (self.fdyn(self.state + k3,self.params)  + self.gctr(self.u))*self.dt
+        
+        new_state = self.state + (k1 + 2*k2 + 2*k3 + k4)/6
+        #new_state += np.random.normal(0,1,new_state.shape) * self.dt
+        
+        return new_state
+
+    def integrator(self):
+        k1 = (self.fdyn(self.state,self.params)) * self.dt
+        k2 = (self.fdyn(self.state + .5*k1,self.params))*self.dt
+        k3 = (self.fdyn(self.state + .5*k2,self.params))*self.dt
+        k4 = (self.fdyn(self.state + k3,self.params))*self.dt
         
         new_state = self.state + (k1 + 2*k2 + 2*k3 + k4)/6
         #new_state += np.random.normal(0,1,new_state.shape) * self.dt
@@ -69,19 +82,19 @@ class dyn_sys:
     # Method to run the simulationo along tvect
     def run(self,x_i):
         self.state = x_i
-        self.state_roster = []
+        state_roster = []
 
         for tt,time in enumerate(self.tvect):
-            self.state_roster.append(self.state)
+            state_roster.append(self.state)
             self.state = self.integrator()
         
-        self.state_roster = np.array(self.state_roster).squeeze().T
+        self.state_roster = np.hstack(state_roster)
     
     # Method that does setup for the run, then runs, then does post-run organizing
     def sim(self,x_i,t_end,element=0):
         self.tvect = np.linspace(0,t_end,int(t_end/self.dt))
+        self.u = np.zeros_like(self.tvect)
         self.run(x_i = x_i)
-        self.measured_ts = self.measure(self.state_roster)
     
     # Return the full state trajectory directly
     def states(self):
@@ -110,17 +123,18 @@ class measurement:
 if __name__=='__main__':
     print('Test Run')
     node_N = 10
-    network = nx.erdos_renyi_graph(node_N,0.8)
+    network = nx.erdos_renyi_graph(node_N,0.5)
     L_net = nx.linalg.laplacianmatrix.laplacian_matrix(network).todense()
     #msys = dyn_sys(N=3,L = np.array([[1,2,-0.1],[2,-1,-0.1],[1,1,1]]))
     
     params = {'L': L_net,'c': np.random.uniform(0.1,5,size=(10,1)),'alpha': np.random.uniform(0.1,5,size=(10,1)),
               'D':nx.linalg.graphmatrix.incidence_matrix(network).todense(),
-              'w':1.2,
-              'k':5}
-    msys = dyn_sys(params=params,dynamics = kuramoto,control=zeros)
+              'w':10,
+              'k':10}
+    
+    nx.draw(network)
+    msys = dyn_sys(params=params)
+    msys.set_dynamics(fn=kuramoto)
     x_init = np.random.uniform(-np.pi/2,np.pi/2,size=(node_N,1))
     msys.sim(x_i = x_init,t_end=10)
-    msys.plot_measured()
-    #%%
-    plt.figure();plt.plot(msys.states().T)
+    plt.figure();plt.plot(np.sin(msys.states().T))
