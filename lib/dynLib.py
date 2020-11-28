b319692ad7497dd36b714535f60f935215b7b840
Vineet Tiruvadi <virati@gmail.com>
2020-11-28 16:35:19 -0500

    Wilson-Cowan now coded into dynLib, dynSys basic structure seems robust enough -> next is Jansen-Rit implementation

--- lib/dynLib.py
+++ lib/dynLib.py
@@ -45,8 +45,9 @@ def rand_squar(params,x):
 
 #%% Classes below
 class W_C(dsys):
-    params = {'T_e': 1,
-          'T_i': 1,
+    
+    params = {'T_e': 5,
+          'T_i': 5,
           'beta': {'e':-1,
                  'i':-1},
           'w':{'ee':10,
@@ -57,7 +58,7 @@ class W_C(dsys):
           'thresh':{'e':0.2,
                     'i':4},
           'tau':0,
-          'net_k':0}
+          'net_k':1/10}
     
     def __init__(self,**kwargs):
         super().__init__(d=2,**kwargs)
@@ -110,3 +111,99 @@ class Ksys(dsys):
     def post_integrator(self):
         pass
         self.state = (self.state + np.pi) % (2 * np.pi) - np.pi
+        
+''' The Delay-W-C class for DO Modeling'''
+
+class W_C_d(dsys):
+    
+    params = {'T_e': 5,
+          'T_i': 5,
+          'beta': {'e':-1,
+                 'i':-1},
+          'w':{'ee':10,
+               'ii':3,
+               'ei':12,
+               'ie':8},
+          'alpha':0.1,
+          'thresh':{'e':0.2,
+                    'i':4},
+          'tau':0,
+          'net_k':1/10}
+    
+    def __init__(self,**kwargs):
+        super().__init__(d=2,**kwargs)
+    
+    def set_ctrl(self,u=[]):
+        if u == []: self.u = np.zeros_like(self.tvect)
+        elif u == 'sine': 
+            self.u = 10*np.sin(2 * np.pi * 130 * self.tvect)
+            halfway = int(round(self.u.shape[0]/2))
+            self.u[:halfway] = 0
+        else: self.u = u
+        
+        
+    def fdyn(self,params,x,u=0):
+        e = np.copy(x[:,0]) #region number is first, then element inside
+        i = np.copy(x[:,1]) #region number is first, then element inside
+        
+        tau = params['tau']
+        alpha = params['alpha']
+        beta = params['beta']
+        w = params['w']
+        thresh = params['thresh']
+        net_k = params['net_k']
+        L = params['L']
+        
+        e_dot = np.zeros(shape=(x.shape[0],1))
+        i_dot = np.zeros(shape=(x.shape[0],1))
+        
+        for nn in range(x.shape[0]):
+            e_dot_p = params['T_e'] * (-e[nn] + sigm(-beta['e'] * (e[nn] * w['ee'] - i[nn] * w['ei'] - thresh['e'] + net_k*np.dot(L[nn,:],e))) + u)
+            i_dot_p = params['T_i'] * (-i[nn] + sigm(-beta['i'] * (-i[nn] * w['ii'] + e[nn] * w['ie'] - thresh['i'])))
+            
+            e_dot[nn] = e_dot_p
+            i_dot[nn] = i_dot_p
+            
+        return np.array([e_dot,i_dot]).squeeze().T
+
+        
+    '''What do we do after integrator? This would be where we reset phases, for example'''
+    def post_integrator(self):
+        pass
+        self.state = (self.state + np.pi) % (2 * np.pi) - np.pi
+
+
+
+class JR(dsys):
+    params = {'T_e': 5,
+      'T_i': 5,
+      'beta': {'e':-1,
+             'i':-1},
+      'w':{'ee':10,
+           'ii':3,
+           'ei':12,
+           'ie':8},
+      'alpha':0.1,
+      'thresh':{'e':0.2,
+                'i':4},
+      'tau':0,
+      'net_k':1/10}
+    
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        
+    def fdyn(self,params,x,u=0):
+        x_dot = np.zeros_like(x) #should give you R x D, where R is regions, D is internal dof
+        
+        alpha = params['alpha']
+        
+        x_dot[:,0] = x[3]
+        x_dot[:,1] = x[4]
+        x_dot[:,2] = x[5]
+        x_dot[:,3] = A[0] * alpha * sigm['y1']['y2'] - 2.0 * alpha[0] * y[3] - alpha[0] ** 2 * x[0]
+        x_dot[:,4] = A[0] * a[0] * (mu[0] + a_2[0] * J[0] * sigm['y0']['1'] + c[0] + src[0]) - 2.0 * a[0] * x[4] - a[0] ** 2 * x[1]
+        x_dot[:,5] = B[0] * b[0] * (a_4[0] * J[0] * sigm['y0']['3']) - 2.0 * b[0] * x[5] - b[0] ** 2 * y[2]
+        
+        
+        return x_dot
+    
--- lib/dynSys.py
+++ lib/dynSys.py
@@ -12,7 +12,50 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import pdb
 import scipy.signal as sig
+from mayavi import mlab
 
+'''A simple 'brain network' class that does basic things on top of networkx'''
+class brain_net:
+    G = []
+    
+    def __init__(self,N=10,connectivity=50):
+        self.G = nx.gnm_random_graph(N,connectivity)
+        
+    def laplacian(self):
+        return nx.linalg.laplacian_matrix(self.G).todense()
+    
+    def plot(self):
+        plt.figure()
+        nx.draw(self.G)
+        
+        
+    def render_graph(self):
+        # reorder nodes from 0,len(G)-1
+        G=nx.convert_node_labels_to_integers(self.G)
+        # 3d spring layout
+        pos=nx.spring_layout(G,dim=3)
+        # numpy array of x,y,z positions in sorted node order
+        xyz=np.array([pos[v] for v in sorted(G)])
+        # scalar colors
+        scalars=np.array(G.nodes())+5
+        
+        mlab.figure(1, bgcolor=(0, 0, 0))
+        mlab.clf()
+        
+        pts = mlab.points3d(xyz[:,0], xyz[:,1], xyz[:,2],
+                            scalars,
+                            scale_factor=0.1,
+                            scale_mode='none',
+                            colormap='Blues',
+                            resolution=20)
+        
+        pts.mlab_source.dataset.lines = np.array(G.edges())
+        tube = mlab.pipeline.tube(pts, tube_radius=0.01)
+        mlab.pipeline.surface(tube, color=(0.8, 0.8, 0.8),opacity=0.1)
+        
+        #mlab.savefig('mayavi2_spring.png')
+        #mlab.show() # interactive window
+    
 
 class dsys:
     state = []
