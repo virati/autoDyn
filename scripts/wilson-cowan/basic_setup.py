0b3d098014351744dd3f13bab9c73e7f2a0a7500
Vineet Tiruvadi <virati@gmail.com>
2022-06-04 23:20:52 -0700

    progress in implementing WC

--- scripts/wilson-cowan/basic_setup.py
+++ scripts/wilson-cowan/basic_setup.py
@@ -1,7 +1,12 @@
 #%%
 from autodyn.core import dynamical as dyn
-from autodyn.models.canonical.standard import lorenz, consensus, single_hopf
+from autodyn.models.neuro.wc import wc_drift
 from autodyn.core.network import connectivity
 
 #%%
-wilson_cowan = dyn.system(wc_vanilla)
+wilson_cowan = dyn.system(wc_drift)
+param_set = 
+wilson_cowan.simulate(T=100, dt=0.1, params=param_set)
+wilson_cowan.plot_phase()
+
+# %%
--- src/autodyn/models/neuro/wc.py
+++ src/autodyn/models/neuro/wc.py
@@ -5,9 +5,11 @@ def sigm(x):
     return 1 / (1 + np.exp(-x))
 
 
-def wc_drift(params, x, u=0):
-    e = np.copy(x[:, 0])  # region number is first, then element inside
-    i = np.copy(x[:, 1])  # region number is first, then element inside
+def wc_drift(x, **kwargs):
+    e = x[0]  # region number is first, then element inside
+    i = x[1]  # region number is first, then element inside
+
+    params = kwargs["params"]
 
     tau = params["tau"]
     alpha = params["alpha"]
@@ -19,27 +21,11 @@ def wc_drift(params, x, u=0):
 
     e_dot = np.zeros(shape=(x.shape[0], 1))
     i_dot = np.zeros(shape=(x.shape[0], 1))
+    e_dot_p = params["T_e"] * (
+        -e + sigm(-beta["e"] * (e * w["ee"] - i * w["ei"] - thresh["e"]))
+    )
+    i_dot_p = params["T_i"] * (
+        -i + sigm(-beta["i"] * (-i * w["ii"] + e * w["ie"] - thresh["i"]))
+    )
 
-    for nn in range(x.shape[0]):
-        e_dot_p = params["T_e"] * (
-            -e[nn]
-            + sigm(
-                -beta["e"]
-                * (
-                    e[nn] * w["ee"]
-                    - i[nn] * w["ei"]
-                    - thresh["e"]
-                    + net_k * np.dot(L[nn, :], e)
-                )
-            )
-            + u
-        )
-        i_dot_p = params["T_i"] * (
-            -i[nn]
-            + sigm(-beta["i"] * (-i[nn] * w["ii"] + e[nn] * w["ie"] - thresh["i"]))
-        )
-
-        e_dot[nn] = e_dot_p
-        i_dot[nn] = i_dot_p
-
-    return np.array([e_dot, i_dot]).squeeze().T
+    return np.array([e_dot_p, i_dot_p]).squeeze().T
