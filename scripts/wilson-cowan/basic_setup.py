8d154565b44c739ed741b39a9cd01afd06f5e5af
Vineet Tiruvadi <virati@gmail.com>
2022-06-04 23:08:37 -0700

    trying to get wilson-cowan working again

--- scripts/wilson-cowan/basic_setup.py
+++ scripts/wilson-cowan/basic_setup.py
@@ -1,1 +1,7 @@
+#%%
 from autodyn.core import dynamical as dyn
+from autodyn.models.canonical.standard import lorenz, consensus, single_hopf
+from autodyn.core.network import connectivity
+
+#%%
+wilson_cowan = dyn.system(wc_vanilla)
