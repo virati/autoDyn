8ea2a0c1a4abaa9329ab5d9d865cfe6c24d54f8c
Vineet Tiruvadi <virati@gmail.com>
2022-08-05 00:52:44 +0000

    initial implement of legendre

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="autodyn",
     version="0.1.4",
-    description="A module for differentiable dynamical systems",
+    description="A library for differentiable dynamical systems",
     author="Vineet Tiruvadi",
     author_email="virati@gmail.com",
     package_dir={"": "src"},
---
+++ src/autodyn/core/lifters/legendre.py
@@ -0,0 +1,24 @@
+from typing import Optional
+import jax.numpy as np
+import numpy as nnp
+
+
+def L_lift(x):
+
+    a * x + a * x**3
+
+
+def lift(
+    trajectory: np.ndarray,
+    M: int,
+    U: Optional[np.ndarray] = None,
+):
+    if trajectory.ndim != 2:
+        raise NotImplemented
+
+    N, T = trajectory.shape
+    if U is None:
+        U = nnp.random.multivariate_normal(nnp.zeros(M), nnp.eye(M, N), size=(M, N))
+
+    # need an array of callables
+    U @ f(trajectory)
