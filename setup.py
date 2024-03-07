fdc581f6f91d01815ff09c0ca254fda16445c286
Vineet Tiruvadi <virati@gmail.com>
2024-03-07 14:29:29 -0800

    upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="autodyn",
-    version="0.1.4",
+    version="0.1.5",
     description="A module for differentiable dynamical systems",
     author="Vineet Tiruvadi",
     author_email="virati@gmail.com",
