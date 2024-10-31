2a31977a699ffd750f4cc2863a1a313e62ca1583
Vineet Tiruvadi <virati@gmail.com>
2024-10-31 04:52:04 +0000

    added generics

---
+++ src/autodyn/models/generics.py
@@ -0,0 +1,22 @@
+import typing
+
+class network:
+    def __init__(self):
+        pass
+
+    def set_connectivity(self, input_connectivity):
+        pass
+
+    def set_drift(self, input_drift_function):
+        pass
+
+    def set_region_euclidean(self, input_region_coords):
+        pass
+
+    def set_tract_euclidean(self, input_tract_coords):
+        pass
+
+    def set_euclideans(self, input_anatomy):
+        self.set_region_euclidean(input_anatomy['regions'])
+        self.set_tract_euclidean(input_anatomy['tracts'])
+        
